ffbe2f2d3fd39cfc000807a8478270d5fb43c1c3
dunedains <fe.zapatao@duocuc.cl>
2026-05-20 00:15:21 -0400

    fix feign client property names

--- src/main/java/com/ecommers/whitelist/client/ProductClient.java
+++ src/main/java/com/ecommers/whitelist/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "${feign.client.product-url}")
+@FeignClient(name = "product-service", url = "${feign.client.product_url}")
 public interface ProductClient {
 
     @GetMapping("/api/productos/{id}")
